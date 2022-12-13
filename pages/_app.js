557a211055e9b37e62dbd7e682e94c13261583d0
Shashwat Jain <shashwatjain_ec21a18_23@dtu.ac.in>
2022-12-14 00:03:35 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import './styles.css';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <title>AI Tweeter</title>
+        <meta name='description' content='Write Viral Tweet Threads Using AI'/>
+      </Head>
+     <Component {...pageProps} />
+    </>
+  )
 }
 export default App;
