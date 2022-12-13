28abdc97eaf62a57e55380eb6413eb5f8f59a91e
Shashwat Jain <shashwatjain_ec21a18_23@dtu.ac.in>
2022-12-13 23:45:11 +0530

    update

---
+++ pages/api/generate.js
@@ -0,0 +1,23 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `write a viral twitter thread optimized to get good engagement on the topic: `;
+const generateAction = async (req, res) => {
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.85,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,40 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import twitterLogo from '../assets/twitter-logo.png';
 
 const Home = () => {
+
+  const [userInput, setUserInput] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,13 +43,39 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Viral Tweet Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Just give a topic and get a twitter thread bound to go viral!</h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea placeholder="building in public" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+          <div className="prompt-buttons">
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : (apiOutput==="") ? <p>Generate</p> : <p>Regenerate</p>}
+    </div>
+  </a>
+</div>
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Your Viral Thread</h3>
       </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+        </div>
+      </div>
+      <div class="grow-containers">
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
@@ -30,6 +88,26 @@ const Home = () => {
           </div>
         </a>
       </div>
+      <div className="badge-container-2 grow">
+        <a
+          href="https://twitter.com/whatshashwhat"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image className='twitter-img' src={twitterLogo} alt="Twitter logo" />
+            <p>@WhatShashwhat</p>
+          </div>
+        </a>
+      </div>
+      </div>
+    <div class="extension">
+      <p>Like this tool?</p>
+      <div class="extension-link-container">
+      <a className="extension-link" href="#">Download the Extension</a>
+      <p>and use directly in Twitter</p>
+      </div>
+    </div>  
     </div>
   );
 };
